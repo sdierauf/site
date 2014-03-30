8fa8c9510e6e9783425d29b940162740abb266eb
sdierauf <sdierauf@gmail.com>
2014-03-30 08:49:32 -0600

    added console messages, deleted some tests

--- bashrack.js
+++ bashrack.js
@@ -4,17 +4,19 @@ store = 'Im stored in store';
 
 net.createServer(function (socket) {
   //on connect, save bash to a variable
-  socket.write("Cool, connected to port 1111\n");
+  //socket.write("Cool, connected to port 1111\n");
   socket.write(store);
+  console.log(socket.remoteAddress + " downloaded bashrc\n");
 
 }).listen(1111);
 
 net.createServer(function (socket) {
-  socket.write("Cool, connected to port 2222");
+  //socket.write("Cool, connected to port 2222");
   
   socket.on('data', function (data) {
-    socket.write(data+"\n");
+    //socket.write(data+"\n");
     store = String(data);
+    console.log(socket.remoteAddress + " uploaded bashrc\n");
   });
 
 }).listen(2222);
