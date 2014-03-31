a1de6ac150e76921079d62b77f90a5d8950d4d53
sdierauf <sdierauf@gmail.com>
2014-03-31 13:04:55 -0700

    migrated bashrack to normal app

--- app3000.js
+++ app3000.js
@@ -52,3 +52,31 @@ http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
+var net = require('net');
+
+store = 'Im stored in store';
+
+net.createServer(function (socket) {
+  //on connect, save bash to a variable
+  //socket.write("Cool, connected to port 1111\n");
+  socket.write(store);
+  console.log(socket.remoteAddress + " downloaded bashrc\n");
+
+}).listen(1111);
+
+net.createServer(function (socket) {
+  //socket.write("Cool, connected to port 2222");
+  
+  socket.on('data', function (data) {
+    //socket.write(data+"\n");
+    store = String(data);
+    console.log(socket.remoteAddress + " uploaded bashrc\n");
+  });
+
+}).listen(2222);
+
+console.log("servers started on 1111 and 2222");
+
+app.get('/bashrack', function(req, res) {
+	res.send(store);
+});
